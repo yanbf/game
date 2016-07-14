5c5d6fed48dde68bf388816e6bb7bfcd4e4a27ac
Bofei Yan <yanbofei@outlook.com>
2016-07-14 22:23:23 +0800

    fix app bug

--- game_world/model/model.py
+++ game_world/model/model.py
@@ -1,4 +1,4 @@
-from app import db
+from game  import db
 
 from sqlalchemy import (
     Table,
--- game_world/views/user.py
+++ game_world/views/user.py
@@ -10,11 +10,8 @@ from flask import (
     flash
 )
 from werkzeug.contrib.cache import SimpleCache
-from app import app
-from model.model import (
-    db,
-    GamerTester
-)
+from game import app
+from model.model import GamerTester
 
 cache = SimpleCache()
 # rv = cache.get(key)
@@ -55,10 +52,6 @@ def logout():
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('user.show_entries'))
 
